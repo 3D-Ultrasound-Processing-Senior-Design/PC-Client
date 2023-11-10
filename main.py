c956510c98a814c1605a281b03bae1e02754df1f
Ian-Flury <ianflury123@gmail.com>
2023-11-09 21:49:57 -0800

    add event loop

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import PySimpleGUI as sg
 
-sg.Window(title="Hello World", layout=[[]], margins=(200, 100)).read()
+window_title = "IMU GUI"
+button_text = "close window"
+
+button = [[sg.Button(button_text)]]
+
+window = sg.Window(title=window_title, layout=button, margins=(800, 400))
+
+# Event loop
+while True:
+    event, values = window.read()
+    if event == button_text or event == sg.WIN_CLOSED:
+        break
+
+window.close()
