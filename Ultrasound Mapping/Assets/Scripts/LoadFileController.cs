59a27a47115f1ba0caed8f39120623911b893279
EmmettCrawfordGU <ecrawford@zagmail.gonzaga.edu>
2024-01-31 16:49:56 -0800

    Initial Unity

---
+++ Ultrasound Mapping/Assets/Scripts/LoadFileController.cs
@@ -0,0 +1,108 @@
+// -----SOURCE---------
+// Author: yasirkula
+// Date Accessed: 1/28/2024
+// Title: Runtime file browser
+// Link: https://github.com/yasirkula/UnitySimpleFileBrowser
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using SimpleFileBrowser;
+
+public class LoadFileController : MonoBehaviour
+{
+    //public static bool ShowSaveDialog( OnSuccess onSuccess, OnCancel onCancel, PickMode pickMode, bool allowMultiSelection = false, string initialPath = null, string initialFilename = null, string title = "Save", string saveButtonText = "Save" );
+    //public static bool ShowLoadDialog( OnSuccess onSuccess, OnCancel onCancel, PickMode pickMode, bool allowMultiSelection = false, string initialPath = null, string initialFilename = null, string title = "Load", string loadButtonText = "Select" );
+    //public string FilePath;
+    public delegate void OnSuccess( string[] paths );
+    public delegate void OnCancel();
+	public static string destinationPath;
+    // Start is called before the first frame update
+    void Start()
+	{
+		// Set filters (optional)
+		// It is sufficient to set the filters just once (instead of each time before showing the file browser dialog), 
+		// if all the dialogs will be using the same filters
+        
+        //USE THIS TO CHANGE FILTERS
+		FileBrowser.SetFilters( true, new FileBrowser.Filter( "Ultrsound Scan File", ".csv" ) );
+
+		// Set default filter that is selected when the dialog is shown (optional)
+		// Returns true if the default filter is set successfully
+		// In this case, set Images filter as the default filter
+		FileBrowser.SetDefaultFilter( ".csv" );
+
+		// Set excluded file extensions (optional) (by default, .lnk and .tmp extensions are excluded)
+		// Note that when you use this function, .lnk and .tmp extensions will no longer be
+		// excluded unless you explicitly add them as parameters to the function
+		FileBrowser.SetExcludedExtensions( ".lnk", ".tmp", ".zip", ".rar", ".exe" );
+
+		// Add a new quick link to the browser (optional) (returns true if quick link is added successfully)
+		// It is sufficient to add a quick link just once
+		// Name: Users
+		// Path: C:\Users
+		// Icon: default (folder icon)
+		FileBrowser.AddQuickLink( "Users", "C:\\Users", null );
+
+		// Show a save file dialog 
+		// onSuccess event: not registered (which means this dialog is pretty useless)
+		// onCancel event: not registered
+		// Save file/folder: file, Allow multiple selection: false
+		// Initial path: "C:\", Initial filename: "Screenshot.png"
+		// Title: "Save As", Submit button text: "Save"
+		// FileBrowser.ShowSaveDialog( null, null, FileBrowser.PickMode.Files, false, "C:\\", "Screenshot.png", "Save As", "Save" );
+
+		// Show a select folder dialog 
+		// onSuccess event: print the selected folder's path
+		// onCancel event: print "Canceled"
+		// Load file/folder: folder, Allow multiple selection: false
+		// Initial path: default (Documents), Initial filename: empty
+		// Title: "Select Folder", Submit button text: "Select"
+		// FileBrowser.ShowLoadDialog( ( paths ) => { Debug.Log( "Selected: " + paths[0] ); },
+		//						   () => { Debug.Log( "Canceled" ); },
+		//						   FileBrowser.PickMode.Folders, false, null, null, "Select Folder", "Select" );
+
+		// Coroutine example
+		StartCoroutine( ShowLoadDialogCoroutine() );
+	}
+
+    IEnumerator ShowLoadDialogCoroutine()
+	{
+		// Show a load file dialog and wait for a response from user
+		// Load file/folder: both, Allow multiple selection: true
+		// Initial path: default (Documents), Initial filename: empty
+		// Title: "Load File", Submit button text: "Load"
+		yield return FileBrowser.WaitForLoadDialog( FileBrowser.PickMode.FilesAndFolders, true, null, null, "Load Files and Folders", "Load" );
+
+		// Dialog is closed
+		// Print whether the user has selected some files/folders or cancelled the operation (FileBrowser.Success)
+		//Debug.Log( FileBrowser.Success );
+		
+
+		if( FileBrowser.Success )
+		{
+			// Print paths of the selected files (FileBrowser.Result) (null, if FileBrowser.Success is false)
+			for( int i = 0; i < FileBrowser.Result.Length; i++ )
+				Debug.Log( FileBrowser.Result[i] );
+				//FilePath = FileBrowser.Result[i];
+
+			// Read the bytes of the first file via FileBrowserHelpers
+			// Contrary to File.ReadAllBytes, this function works on Android 10+, as well
+			byte[] bytes = FileBrowserHelpers.ReadBytesFromFile( FileBrowser.Result[0] );
+
+			// Or, copy the first file to persistentDataPath
+			destinationPath = Path.Combine( Application.persistentDataPath, FileBrowserHelpers.GetFilename( FileBrowser.Result[0] ) );
+			FileBrowserHelpers.CopyFile( FileBrowser.Result[0], destinationPath );
+            Debug.Log(destinationPath);
+			LoadScanController sc = gameObject.GetComponent<LoadScanController>();
+			//sc.OpenFile();
+		}
+	}
+}
+
+
+
+
---
+++ Ultrasound Mapping/Assets/Scripts/LoadScanController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+//  public class Scan{
+//         public string Name {get; set;}
+//         public Angle = new float[3];
+//         public Position = new float[3];
+
+// }
+
+public class LoadScanController : MonoBehaviour
+{
+    //List<Scan> scanList = new List<Scan>();
+    //List<string> listB = new List<string>();
+   
+    //Scan tempScan;
+
+    // //private LoadFileController path = gameObject.<GetComponent<LoadFileController>();
+    // private static string path;
+    // public void OpenFile(){
+    //     path = LoadFileController.destinationPath;
+    // using(var reader = new StreamReader(path))
+    //     {
+    //         while (!reader.EndOfStream)
+    //         {
+    //             var line = reader.ReadLine();
+    //             var values = line.Split(',');
+    //             values[0] = tempScan.Name; // add name category
+    //             values[1] = tempScan.Angle[0]; // populate scan 
+    //             values[2] = tempScan.Angle[1];
+    //             values[3] = tempScan.Angle[2];
+
+    //             values[4] = tempScan.Position[0];
+    //             values[5] = tempScan.Position[1];
+    //             values[6] = tempScan.Position[2];
+
+    //             scanList.Add(tempScan);
+    //         }
+    //     }
+        
+    // }
+}
