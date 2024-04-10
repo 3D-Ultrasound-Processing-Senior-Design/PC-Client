f08dbb63faa080878ece68e80a40d7ca70aafe3e
Amelia-E <43151783+Amelia-E@users.noreply.github.com>
2024-04-10 12:51:22 -0700

    trying to get the how to better

--- Ultrasound Mapping/Assets/Scripts/HowToController.cs
+++ Ultrasound Mapping/Assets/Scripts/HowToController.cs
@@ -15,32 +15,28 @@ public class HowToController : MonoBehaviour
     private int num;
     void Start()
     {
-        num = 1;
+        num = 0;
     }
     public void ButtonDemo()
     {
+        num ++;
         switch(num)
         {
             case 0:
                 instructions.GetComponent<TextMeshProUGUI>().text = "Welcome. The Goal of this program is to help you refind the orientation of your ultrasound probe when making multiple scans. Press the next button when you are ready to continue:";
-                num++;
                 break; 
             case 1:
                 photo.sprite = MainMenuSprite;
                 instructions.GetComponent<TextMeshProUGUI>().text = "The main menu consists of multiple options,\n click next to continue";
-                num++;
                 break;                
             case 2:
                 instructions.GetComponent<TextMeshProUGUI>().text = "The first time you scan a location, you will click 'New Scan' \n click next to continue";
-                num++;
                 break;
             case 3:
                 instructions.GetComponent<TextMeshProUGUI>().text = "Place Holder,\n click next to continue";
-                num++;
                 break;
             default:
                 instructions.GetComponent<TextMeshProUGUI>().text = "Place Holder,\n click next to continue";
-                num++;
                 break;
         }
     
