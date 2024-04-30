e0f3b6c3a3c2aae4c55bba6497503d5dc58b0927
Amelia-E <43151783+Amelia-E@users.noreply.github.com>
2024-04-30 14:14:34 -0700

    Pics Add
    -pics added
    -the text still needs to be edited

--- Ultrasound Mapping/Assets/Scripts/HowToController.cs
+++ Ultrasound Mapping/Assets/Scripts/HowToController.cs
@@ -12,7 +12,7 @@ public class HowToController : MonoBehaviour
     public TextMeshProUGUI instructions;
     public Image photo;
     public Sprite MainMenuSprite;
-    public Sprite sptite1,sprite2,sprite3, sprite4, sprite5, sprite6, sprite7, sprite8, sprite9;
+    public Sprite sptite1,sprite2,sprite3, sprite4, sprite5, sprite6, sprite7, sprite8, sprite9, sprite10, sprite11;
     public Button PreviousButton, HomeButton, NextButton;
     //public TMP_text 
     
@@ -21,7 +21,7 @@ public class HowToController : MonoBehaviour
     public const string inst2 = "Begin by attaching the IMU onto the ultrasound Probe. Place the probe where you plan on setting the origin of the IMU, and plug the IMU into the computer using a USB cable. Press the Next button when you are ready to continue";
     public const string inst3 = "To begin a scan one would select the 'Scan' button. Press the Next button when you are ready to continue";
     public const string inst4 = "This screen will take a few seconds to recognize that the IMU is connected. Once it recognizes the connection the model of the ultrasound probe will begin to move as you move it in space. There is also an indicator on the right side that will indicate when the IMU is connected";
-    public const string inst5 = "To begin taking New Scan, the first step is to zero out the IMU. Place the probe where you would like to set the origin of the orientation. It is important to remember where this position is for if you need to re-zero the probe before replicating the scan. Press the ‘Zero’ button";
+    public const string inst5 = "To begin taking New Scan, the first step is to zero out the IMU. Place the probe where you would like to set the origin of the orientation. It is important to remember where this position is, in order to re-zero the probe before replicating the scan. Press the ‘Zero’ button";
     public const string inst6 = "Now you may take the ultrasound scan as you normally would. When you have the image you want on the ultrasound machine, press the ‘Save Scan’ button. This will save the orientation of the probe at the time the button is clicked.";
     public const string inst7 = "This button will bring up a file explorer, where you can set the name of the .csv file that you save the orientation in. Name the file what you would like to, and then click save";
     public const string inst8 = "When you are ready to recreate the scan, place the probe in the position and location in which you zeroed the IMU on your initial scan. Press the ‘Zero’ button.";
@@ -43,38 +43,47 @@ public class HowToController : MonoBehaviour
         {
             case inst1:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst2;
+                photo.sprite = MainMenuSprite;
                 break;  
             case inst2:
-                photo.sprite = MainMenuSprite;
+                photo.sprite = sprite3;
                 instructions.GetComponent<TextMeshProUGUI>().text = inst3;
                 break;                
             case inst3:
-                photo.sprite = sprite3;
+                photo.sprite = sprite4;
                 instructions.GetComponent<TextMeshProUGUI>().text = inst4;
                 break;
             case inst4:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst5;
+                photo.sprite = sprite5;
                 break;
             case inst5:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst6;
+                photo.sprite = sprite6;
                 break;
             case inst6:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst7;
+                photo.sprite = sprite7;
                 break;
             case inst7:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst8;
+                photo.sprite = sprite8;
                 break;
             case inst8:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst9;
+                photo.sprite = sprite9;
                 break;
             case inst9:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst10;
+                photo.sprite = sprite10;
                 break;
             case inst10:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst11;
+                photo.sprite = sprite11;
                 break;
             default:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst1;
+                photo.sprite = MainMenuSprite;
                 break;
         }
     
@@ -88,6 +97,7 @@ public class HowToController : MonoBehaviour
         {
             case inst2:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst1;
+                photo.sprite = MainMenuSprite;
                 break; 
 
             case inst3:
@@ -96,30 +106,39 @@ public class HowToController : MonoBehaviour
                 break;                
             case inst4:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst3;
+                photo.sprite = sprite3;
                 break;
             case inst5:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst4;
+                photo.sprite = sprite4;
                 break;
             case inst6:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst5;
+                photo.sprite = sprite5;
                 break;
             case inst7:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst6;
+                photo.sprite = sprite6;
                 break;
             case inst8:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst7;
+                photo.sprite = sprite7;
                 break;
             case inst9:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst8;
+                photo.sprite = sprite8;
                 break;
             case inst10:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst9;
+                photo.sprite = sprite9;
                 break;
             case inst11:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst10;
+                photo.sprite = sprite10;
                 break;
             default:
                 instructions.GetComponent<TextMeshProUGUI>().text = inst1;
+                photo.sprite = MainMenuSprite;
                 break;
         }
     }
